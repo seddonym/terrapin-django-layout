5f82d1bde1c387301b15008125e35a7b232d8a5b
David Seddon <david@pepperpotdesign.co.uk>
2013-10-31 15:33:39 +0000

    Tweaked more robust setup

--- project_name/libs/environment/__init__.py
+++ project_name/libs/environment/__init__.py
@@ -1,20 +1,20 @@
 import os
 import sys
+import site
 import socket
-from {{ project_name }}.settings.environments import ENVIRONMENTS, \
-                                                    MONITOR_ENVIRONMENTS
+from {{ project_name }}.settings.environments import ENVIRONMENTS
 
 
 # Get the path to the project based on the location of
 # this file
 project_path = '/'.join(__file__.split('/')[:-4])
-settings_file = ENVIRONMENTS[socket.gethostname()][project_path]
+settings = ENVIRONMENTS[socket.gethostname()][project_path]
 
 
 def _setup_environment_vars():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE",
                       "{{ project_name }}.settings.%s" %
-                      settings_file)
+                      settings['settings_file'])
 
 
 def wsgi_environment():
@@ -25,7 +25,14 @@ def wsgi_environment():
     from django.core.wsgi import get_wsgi_application
     application = get_wsgi_application()
 
-    if settings_file in MONITOR_ENVIRONMENTS:
+    if settings.get('virtualenv'):
+        # This makes sure the virtualenv is activated
+        # for any virtualenv environments
+        site.addsitedir(os.path.join(settings['virtualenv'], 'lib/python2.7/site-packages'))
+        activate_this = os.path.join(settings['virtualenv'], 'bin/activate_this.py')
+        execfile(activate_this, dict(__file__=activate_this))
+
+    if settings.get('monitor'):
         from {{ project_name }}.libs import monitor
         monitor.start(interval=1.0)
 
--- project_name/settings/environments.py
+++ project_name/settings/environments.py
@@ -5,21 +5,43 @@
 # The first set of keys are for the machines' host names, as output by a call
 # to socket.gethostname().
 # For each machine, there is a dict that is keyed with the working directory
-# that manage.py and wsgi.py will be running from, with values that are the
-# name of the settings file (less the .py extension).
+# that manage.py and wsgi.py will be running from, with a dict that stores
+# the following:
+#   'settings_file': the name of the settings file (less the .py extension)
+#   'virtualenv': optional, the path to the virtual env,
+#                 if it needs to be activated
+#   'monitor': optional, whether or not we should monitor for code changes
 #
-# environments ={
-#     'HOSTNAME_ONE': {'/path/to/local/directory': 'local'},
+# environments = {
+#     'HOSTNAME_ONE': {
+#            '/path/to/local/directory': {
+#                'settings_file': 'local',
+#            },
+#     },
 #     'HOSTNAME_TWO': {
-#         '/path/to/other/directory': 'SETTINGS_FILE_NAME'
+#            '/path/to/live/directory': {
+#                'settings_file': 'live',
+#                'virtual_env': '/path/to/virtualenv/',
+#            },
 #     },
 # }
 
 ENVIRONMENTS = {
-    'lanky': {'/home/david/www/{{ project_name }}': 'local'},
-    'ketchup': {'/home/davidseddon/webapps/{{ project_name }}_live/{{ project_name }}': 'live',
-                '/home/davidseddon/webapps/{{ project_name }}_dev/{{ project_name }}': 'dev'},
-}
 
-# List any environments which we should monitor for code changes
-MONITOR_ENVIRONMENTS = ('local',)
+    'lanky': {
+
+        '/home/david/www/{{ project_name }}':
+            {'settings_file': 'local', 'monitor': True},
+    },
+
+    'ketchup': {
+
+        '/home/davidseddon/webapps/{{ project_name }}_live/{{ project_name }}':
+            {'settings_file': 'live',
+             'virtualenv': '/home/davidseddon/.virtualenvs/{{ project_name }}_live'},
+
+        '/home/davidseddon/webapps/{{ project_name }}_dev/{{ project_name }}':
+            {'settings_file': 'dev',
+             'virtualenv': '/home/davidseddon/.virtualenvs/{{ project_name }}_dev'},
+    }
+}
